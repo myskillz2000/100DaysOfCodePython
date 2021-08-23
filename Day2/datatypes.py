b70a6f438b69423c3ce95465ed8a3c3ccd1ad249
myskillz2000 <matthewstiller484@yahoo.com>
2021-08-23 18:55:25 -0400

    Sample of data types for string, integer, float, and boolean

--- Day2/datatypes.py
+++ Day2/datatypes.py
@@ -1,3 +1,25 @@
 """ This is going to be a file to show different datatypes.
 We will use these datatypes to be able to create a tip
 calculator."""
+
+# Data Types
+
+# String
+
+print("Hello"[0])
+print("Hello"[-1])
+print("Hello"[:])
+
+# Integer
+
+print(123 + 345)
+
+
+# Float
+
+print(123.45)
+
+#Boolean
+
+True
+False
