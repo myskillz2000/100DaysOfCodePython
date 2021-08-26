fcddb9b8386ddedd9078a1af07b66c3b337be499
myskillz2000 <matthewstiller484@yahoo.com>
2021-08-26 02:34:58 -0400

    Add two numbers together.

---
+++ Day2/addnumbers.py
@@ -0,0 +1,12 @@
+"""Add the first and second digits together.
+This will not work if people input non-numbers though,
+there is no validation or try block."""
+
+two_digit_number = input("Type a two digit number:")
+
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[-1])
+
+result = first_digit + second_digit
+
+print(result)
