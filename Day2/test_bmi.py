4d6cf7c1a722e4912c1262f54a381c6135f5717c
myskillz2000 <matthewstiller484@yahoo.com>
2021-10-14 18:07:33 -0400

    Created the test file for bmi calculator.

---
+++ Day2/test_bmi.py
@@ -0,0 +1,7 @@
+import bmi
+
+def test_calc_bmi():
+    assert calc_bmi(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: "1.63")
+    monkeypatch.setattr('builtins.input', lambda _: "60")
+    assert datatypes.input_then_add() == 22.56)
