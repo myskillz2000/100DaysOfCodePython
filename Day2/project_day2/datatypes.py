e50cc7dbf4bbb8ec48dcbcbb58029202379459e3
myskillz2000 <matthewstiller484@yahoo.com>
2021-10-15 10:54:11 -0400

    Added necessry files to make this a module.

---
+++ Day2/project_day2/datatypes.py
@@ -0,0 +1,11 @@
+def add(x,y):
+    return x+y
+
+def input_then_add():
+    num = input("type a two digit number please: ")
+    val = add(int(num[0]),int(num[1]))
+    return val
+
+answer = input_then_add()
+print(answer)
+
