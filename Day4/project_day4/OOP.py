eb6ee7b4ecd2137708623d5ed8f596aa93c2e0f6
myskillz2000 <matthewstiller484@yahoo.com>
2021-10-29 11:49:35 -0400

    Changed the class using magic method.

--- Day4/project_day4/OOP.py
+++ Day4/project_day4/OOP.py
@@ -1,10 +1,15 @@
+
+# A class to create the product
 class Item:
-    def calculate_total(self):
-        pass
+    def __init__(self, name):
+
+    def calculate_total(self, x, y):
+        return x * y
 
 
 
-item1 = Item()
-item1.name = "Phone"
+item1 = Item("Phone")
 item1.price = 100
 item1.quantity = 5
+total = item1.calculate_total(item1.price, item1.quantity)
+print(total)
