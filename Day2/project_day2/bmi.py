3ca339e769c720332592a74a8e9aa51bc235f58e
Matt Stiller <MStiller@ocslog.com>
2022-03-07 09:39:17 -0500

    Got rid of tests that didn't work currently just one test working.

--- Day2/project_day2/bmi.py
+++
@@ -1,7 +0,0 @@
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
-
-def to_bmi(height, weight):
-    height = int(height)*int(height)
-    weight = int(weight)
-    return weight/height
--- Day2/project_day2/test/test_bmi.py
+++
@@ -1,7 +0,0 @@
-from project_day2 import bmi
-
-height = input("What is your height in meters? ")
-weight = input("What is your weight in kg? ")
-
-def test_to_bmi(height, weight):
-    assert bmi.to_bmi(height,weight) == 25.66
